892be65a7ba995089a659b23dede5ac6d487f289
Victor Adamson <a20vicad@student.his.se>
2023-11-29 16:13:16 +0100

    Restructured and added comments

--- Problem2/Problem2.cpp
+++ Problem2/Problem2.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <list>
 using namespace std;
 
 class Graph {
@@ -47,7 +48,7 @@ int Graph::getNumNodes() {
 
 void Graph::printGraph() {
     for (int v = 0; v < V; ++v) {
-        cout << "\n Adjacency list of node " << v << "\n head ";
+        cout << "\nAdjacency list of node " << v << "\n head ";
         vector<int>::iterator i;
         for (i = adj[v].begin();
             i != adj[v].end(); ++i) {
@@ -57,19 +58,22 @@ void Graph::printGraph() {
 }
 
 int main(){
-    queue<int>;
     Graph network(4);
-    vector<bool> visited[4];
-    network.addEdge(0, 1);
-    network.addEdge(1, 2);
-    network.addEdge(1, 3);
-    network.addEdge(3, 1);
-    network.addEdge(3, 2);
-
+    network.addEdge(0, 1); //A dislikes B
+    network.addEdge(1, 2); //B dislikes C
+    network.addEdge(1, 3); //B dislikes D
+    network.addEdge(2, 3); //C dislikes D
+    network.addEdge(2, 1); //C dislikes B
     network.printGraph();
-
+    queue<int> queue;
+    const int i = network.getNumNodes();
+    list<int> nodes[4];
+    cout << "\n" << "Number of nodes: " << i << "\n";
+    vector<bool> visited[4];
+    
 }
 
-bool isAdversary(Graph network) {
+bool isAdversary(Graph network, vector<bool> visited[4]) {
+
     return true;
 }
